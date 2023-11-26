31c5af2d700bde133f09ee33f90e82a39f00d6e6
Rajasekharpera <116863029+Rajasekharpera@users.noreply.github.com>
2023-11-26 20:51:34 +0530

    save

---
+++ src/Person.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+  class Person extends Component{
+    render(){
+        return(
+        <div>
+       <i>
+         <h1>Hello GFG</h1>
+         </i>
+         </div>
+        );
+    }
+
+ }
+ export default Person;
